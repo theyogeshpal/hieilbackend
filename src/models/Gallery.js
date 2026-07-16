2c88f47219ac301aecde4b7565f2887dd197d0c6
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-07-17 00:04:10 +0530

    cjmages

--- src/models/Gallery.js
+++ src/models/Gallery.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   category: String,
   title: String,
+  tagline: String,
   image: String
 }, { timestamps: true });
 module.exports = mongoose.model('Gallery', schema);
